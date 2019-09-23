7b2571dabc31a202e7db17dac3b4dd0812f267a9
Lubynets <lubinets95@gmail.com>
2019-09-23 15:34:00 +0200

    Full-Control sort tracks

--- KFParticle/FullControlFinder.cxx
+++ KFParticle/FullControlFinder.cxx
@@ -7,3 +7,32 @@ void FullControlFinder::Init(const KFPTrackVector &tracks, const KFVertex &pv)
   tracks_ = tracks;
   prim_vx_ = pv;
 }
+
+void FullControlFinder::SortTracks()
+{
+  int Size = tracks_.Size();
+  std::vector<int> trIndex[4];
+  
+  for(int iTr = 0; iTr < Size; iTr++)
+  {
+    if(tracks_.PVIndex()[iTr] < 0)          // secondary
+    {
+      if(tracks_.Q()[iTr] > 0)              // secondary positive
+        trIndex[secPos].push_back(iTr);
+      else
+        trIndex[secNeg].push_back(iTr);     // secondary negative
+    }
+    else                                    // primary
+    {
+      if(tracks_.Q()[iTr] > 0)              // primary positive
+        trIndex[primPos].push_back(iTr);
+      else                                  // primary negative
+        trIndex[primNeg].push_back(iTr);
+    }
+  }
+     
+  for(int iTrType=0; iTrType<4; iTrType++)
+    sorted_tracks_[iTrType].SetTracks(tracks_, trIndex[iTrType], trIndex[iTrType].size());
+  
+  
+}
--- KFParticle/FullControlFinder.h
+++ KFParticle/FullControlFinder.h
@@ -8,8 +8,19 @@ class FullControlFinder{
   virtual ~FullControlFinder() = default;
   
   void Init(const KFPTrackVector &tracks, const KFVertex &pv);
+  void SortTracks();
 
   KFPTrackVector tracks_;
   KFVertex prim_vx_;
-
+  std::vector<KFPTrackVector> sorted_tracks_{4};
+  
+  enum eTrackTypes
+  {
+    secPos = 0,
+    secNeg,
+    primPos,
+    primNeg,
+  };
+  
+  
 };
