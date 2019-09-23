5af418a3fb3cee10a371b0eb79e1b9dd8630df53
Lubynets <lubinets95@gmail.com>
2019-09-23 18:53:02 +0200

    FC tmp

--- KFParticle/FullControlFinder.cxx
+++ KFParticle/FullControlFinder.cxx
@@ -44,5 +44,5 @@ void FullControlFinder::FindParticles()
       if(!(tracks_.PDG()[trIndex_[kSecPos][iSecPos]]==pdg_proton && tracks_.PDG()[trIndex_[kSecNeg][iSecNeg]]==pdg_pionMinus)) continue;
       N++;
     }
-  std::cout << nSecPoses << "\n" << nSecNegs << std::endl;
+  std::cout << nSecPoses << "\t" << nSecNegs << "\t" << N << std::endl;
 }
