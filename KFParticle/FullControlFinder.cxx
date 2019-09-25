5f3921e73a893632aacdcc0e04d9ef9b65773bbe
Lubynets <lubinets95@gmail.com>
2019-09-25 11:42:04 +0200

    tmp commit

--- KFParticle/FullControlFinder.cxx
+++ KFParticle/FullControlFinder.cxx
@@ -119,7 +119,7 @@ void FullControlFinder::FindParticles()
       
       cosine_daughter_pos_ = FindCosMomentumSum(pars1, pars2);
       cosine_daughter_neg_ = FindCosMomentumSum(pars2, pars1);
-      if(cosine_daughter_pos_ > cut_cosine_daughter_pos_ || cosine_daughter_neg_ > cut_cosine_daughter_neg_)
+      if(cosine_daughter_pos_ > cut_cosine_daughter_pos_ || cosine_daughter_neg_ > cut_cosine_daughter_neg_) continue;
 
       N++;
     }
