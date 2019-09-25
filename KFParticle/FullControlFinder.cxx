802b8c772fa506ac98a47c541632072f5bab996d
Lubynets <lubinets95@gmail.com>
2019-09-25 20:30:34 +0200

    tmp cmt

--- KFParticle/FullControlFinder.cxx
+++ KFParticle/FullControlFinder.cxx
@@ -11,19 +11,20 @@ void FullControlFinder::Init(const KFPTrackVector &tracks, const KFVertex &pv)
 
 void FullControlFinder::CancelCuts()
 {
-  cut_chi2_geo_ = 79999.;
-  cut_chi2_prim_neg_ = -79999.;
-  cut_chi2_prim_pos_ = -79999.;
-  cut_chi2_topo_ = 79999.;
-  cut_cosine_daughter_neg_ = -79999.;
-  cut_cosine_daughter_pos_ = -79999.;
-  cut_distance_ = 79999.;
-  cut_l_down_ = -79999.;
-  cut_l_up_ =  79999.;
-  cut_ldl_ = -79999.;
-  cut_ldl_sec_ = -79999.;
-  cut_sigma_mass_ratio_ = 79999.;
-  cut_is_from_pv_ = 79999;
+  float practInf = 1e9;
+  cut_chi2_geo_ = practInf;
+  cut_chi2_prim_neg_ = -practInf;
+  cut_chi2_prim_pos_ = -practInf;
+  cut_chi2_topo_ = practInf;
+  cut_cosine_daughter_neg_ = -practInf;
+  cut_cosine_daughter_pos_ = -practInf;
+  cut_distance_ = practInf;
+  cut_l_down_ = -practInf;
+  cut_l_up_ =  practInf;
+  cut_ldl_ = -practInf;
+  cut_ldl_sec_ = -practInf;
+  cut_sigma_mass_ratio_ = practInf;
+  cut_is_from_pv_ = 799;
 }
 
 void FullControlFinder::SortTracks()
