3015057cd4358b94e97cb5febf5886e6ed8f9653
Lubynets <lubinets95@gmail.com>
2019-09-25 18:10:18 +0200

    tmp cmt

--- KFParticle/FullControlFinder.cxx
+++ KFParticle/FullControlFinder.cxx
@@ -199,7 +199,7 @@ void FullControlFinder::FindParticles()
       
       chi2_topo_ = FindChi2Topo(mother);
       if(chi2_topo_ > cut_chi2_topo_) continue;
-      if(ldl_ < cut_ldl_sec_) continue;
+      //if(ldl_ < cut_ldl_sec_) continue;
       
 
       N++;
