968cda609a1777c5df9ae4ef536ca91a3dcf40de
Lubynets <lubinets95@gmail.com>
2019-10-02 16:47:34 +0200

    tmp cmt

--- KFParticle/FullControlFinder.cxx
+++ KFParticle/FullControlFinder.cxx
@@ -256,7 +256,7 @@ void FullControlFinder::FindParticles()
       if(is_from_pv_ == cut_is_from_pv_) continue;
       if(l_ <= cut_l_down_) continue;
       
-      AnalysisTree::Track Lambda = lambdas_->AddChannel();
+      AnalysisTree::Track* Lambda = lambdas_->AddChannel();
       Lambda->Init( config_->GetBranchConfig( lambdas_->GetId() ) );
 
 
