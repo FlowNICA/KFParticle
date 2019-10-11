1f5e1991c02c556c0d08df6759c5c1dced245689
Lubynets <lubinets95@gmail.com>
2019-10-11 14:49:46 +0200

    add cuts getter-setter

--- KFParticle/FullControlFinder.h
+++ KFParticle/FullControlFinder.h
@@ -25,7 +25,7 @@ class FullControlFinder
   
   const std::vector<float>& GetMass() const {return vec_mass_;};            // TODO remove after debug procedure
   
-  FC_Cuts& GetCuts() const {return cuts_;};                                        // Is it OK to use setter as getter (not constant reference)
+  FC_Cuts& GetCuts() {return cuts_;};                                        // Is it OK to use setter as getter (not constant reference)
   
  protected:
    
